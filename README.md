0db37d2659715491f0a0efa26f57d4d81080c1d7
Viktor Lazarov <vikitor755@gmail.com>
2021-11-11 12:28:15 +0200

    Added command to display all lists. Folder to pre-compile responses on each 'node deploy.js'. Command to display all games. Json file to add games to.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # discord-find-games-discord-bot
 DIscord bot to help people find people to play video games with.
 
+I need to come back to this (user interaction and guild IDs):
+https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot
+
+More complex command options: https://discordjs.guide/interactions/registering-slash-commands.html#subcommands
+
 Discord guide progress - https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot
 
 
@@ -11,10 +16,9 @@ When using the bot, rename the "example-config.json" to "config.json" and set th
 To find the "guildId", turn on Discord developer mode (in the app), right click the server icon on the left and copy that id, that is "guildId".
 
 TODO:
-- Separate the commands in a different folder.
-- Make the commands more maintanable, no if-else, described in the following article( or the next one)
-https://discordjs.guide/creating-your-bot/creating-commands.html#the-problem-with-if-else-if
+- Refactor the file in the "events" folder, see if you need it.
 - test out user interactions
+- If games are changed, redundant roles will need to be removed from the server and the users.
 
 To invite the bot (this may change if the bot needs more or less permissions):
 https://discord.com/api/oauth2/authorize?client_id=907048337155432479&permissions=0&scope=bot%20applications.commands
@@ -22,5 +26,11 @@ https://discord.com/api/oauth2/authorize?client_id=907048337155432479&permission
 Run instructions:
 1. Rename the "example-config.json" file to "config.json", replace with appropriate variables.
 2. Create an invite link for the bot with the "bot" and "application.commands" permissions in the 'Oauth2" tab in the Discord developer portal dashboard.
-3. Run "node commands/deploy.js"
-4. Run "node bot.js".
+3. Run "node deploy.js", to make all commands usable.
+- Every time the "deploy.js" file is run, the new commands become active, no need for restart (tested on Discord API version 9).
+4. Run "node bot.js". To make the bot active
+
+folders:
+- commands - contains one "/" command per file, gets loaded into "bot.js" when it runs.
+- events - contains all events to which the bot listens, one per file.
+- responses - reactions of the bot to all commands, also used to precompile large lists (currently for games), so it can scale and be fast later.
--- bot.js
+++ bot.js
@@ -1,31 +1,53 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+// Loading up the events on which commands will execute
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+// Loading up the commands from the "commands directory."
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+// Message to let us know the bot loads up.
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 
+// Dynamic execution of commands, checking if a message is command and if it has an error sends a generic error message.
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-client.login(token);
-
-// Login to Discord with your client's token
+// Login the bot to Discord, the token is very sensitive data, make sure the "config.json" file is secure.
+// If the credentials are compromised immediately reset them from the Discord developer portal and re-enter them in the "config.json" file.
 client.login(token);
