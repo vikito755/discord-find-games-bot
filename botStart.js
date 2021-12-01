c4c5b3ff3491d7477fc586a8e816b00573cdf1f3
Your Name <you@example.com>
2021-12-01 09:44:50 -0800

    Removed pre-commit hooks from git.

--- botStart.js
+++ botStart.js
@@ -10,15 +10,15 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 // Loading up the events on which commands will execute
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
+// for (const file of eventFiles) {
+// 	const event = require(`./events/${file}`);
+// 	if (event.once) {
+// 		client.once(event.name, (...args) => event.execute(...args));
+// 	}
+// 	else {
+// 		client.on(event.name, (...args) => event.execute(...args));
+// 	}
+// }
 
 // Loading up the commands from the "commands directory."
 for (const file of commandFiles) {
