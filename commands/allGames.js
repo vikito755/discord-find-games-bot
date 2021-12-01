364db483c6cb8ecfb866ecef2660fdcf4bcf6eb3
Your Name <you@example.com>
2021-12-01 13:20:59 -0800

    Husky manual fixes.

--- commands/allGames.js
+++ commands/allGames.js
@@ -2,13 +2,12 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { allGamesResponse } = require('../responses/allGames');
 
 // Displays all available games. In the future categories may be added.
-                                 module.exports = {
+module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('all_games')
-			     	 	 	.setDescription('Display a list of all available games you can get notified for!'),
-	  	 	 	 	 async execute(interaction) {
+		.setDescription('Display a list of all available games you can get notified for!'),
+	async execute(interaction) {
 		// The argument "ephemeral" means that only the user who submitted the command can see the response.
-		 	 await interaction.reply({ content: allGamesResponse, ephemeral: true });
-
+		await interaction.reply({ content: allGamesResponse, ephemeral: true });
 	},
- };
+};
