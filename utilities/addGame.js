ac093620817193a13635470ee6dffbf435e95ad1
Your Name <you@example.com>
2021-12-03 13:15:15 -0800

    Removed an idea to reinitialise the options list every time.

--- utilities/addGame.js
+++ utilities/addGame.js
@@ -7,15 +7,6 @@ const addGame = (gameName, maxPlayers) => {
 	GameStorage.gameOptions.push(createGameOption(gameName));
 	GameStorage.lobbies.push(new GameLobby({ game: gameName, maxPlayers: maxPlayers }));
 
-	// const commands = [];
-	// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-	// for (const file of commandFiles) {
-	// 	const command = require(`./commands/${file}`);
-	// 	commands.push(command.data.toJSON());
-	// }
-	// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-
 };
 
 module.exports.addGame = addGame;
