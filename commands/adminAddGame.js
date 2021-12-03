6abe77a0148aec60b0aa88ca2ac3988ce5d4c402
Your Name <you@example.com>
2021-12-03 13:14:29 -0800

    Add a function that adds a game as an option. It works, but it doesn't push the 'gameOption' changes to the 'GameStorage.jgameOptions' property. I will try rewriting it using 'export'.

--- commands/adminAddGame.js
+++ commands/adminAddGame.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { botManagerRoleId } = require('../constants.json');
+const { addGame } = require('../utilities/addGame');
 
 // Allows admin to add a game option.
 module.exports = {
@@ -8,11 +9,23 @@ module.exports = {
 		.setDescription('Add a game to the list of games.')
 		.addStringOption(option =>
 			option.setName('game')
-				.setDescription('The __**full**__ name of the game you wish to add.')
-				.setRequired(true)),
+				.setDescription('The full name of the game you wish to add.')
+				.setRequired(true))
+		.addIntegerOption(option =>
+			option.setName('max_players')
+				.setDescription('The max number of players for the most popular mode of the game.')
+				.setRequired(true),
+		),
 	async execute(interaction) {
 		const userHasRole = interaction.member._roles.includes(botManagerRoleId);
 		if (userHasRole) {
+
+			const addedGameName = interaction.options._hoistedOptions[0].value;
+			const maxPlayers = interaction.options._hoistedOptions[1].value;
+			addGame(addedGameName, maxPlayers);
+
+			console.log(addedGameName);
+			console.log(maxPlayers);
 			await interaction.reply('Game added by a user with a protected role.');
 		}
 		else {
--- utilities/addGame.js
+++ utilities/addGame.js
@@ -1,12 +1,21 @@
-import { GameStorage } from '../initialisation/GameStorage';
-import { GameLobby } from '../objects/GameLobby';
-import { createGameOption } from './createGameOption';
+const { GameStorage } = require('../initialisation/GameStorage');
+const { GameLobby } = require('../objects/GameLobby');
+const { createGameOption } = require('../utilities/createGameOption');
 
 const addGame = (gameName, maxPlayers) => {
+
 	GameStorage.gameOptions.push(createGameOption(gameName));
 	GameStorage.lobbies.push(new GameLobby({ game: gameName, maxPlayers: maxPlayers }));
+
+	// const commands = [];
+	// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+	// for (const file of commandFiles) {
+	// 	const command = require(`./commands/${file}`);
+	// 	commands.push(command.data.toJSON());
+	// }
+	// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+
 };
 
-export default {
-	addGame,
-};
+module.exports.addGame = addGame;
