a9f11bccb513729437a3d7d1328ebcfc9df72d4c
Your Name <you@example.com>
2021-12-04 01:53:32 -0800

    Updating GameWrapper with a method to update its game options.

--- objects/GamesWrapper.js
+++ objects/GamesWrapper.js
@@ -1,7 +1,7 @@
 /* The purpose of this wrapper is to have the GameLobbies (array of objects) and GameOptions (the options for the game array)
  as its properties so more games can be added and removed without resetting the bot. */
 class GamesWrapper {
-
+// Updating GameWrapper with a method to update its game options.
 	constructor(params) {
 		/* Array of game gameOptions, each option is an array in the following format
         [[game1.name, game1.name], [game2.name, game2.name], , [gameN.name, gameN.name] ] (2 dimensional array) */
